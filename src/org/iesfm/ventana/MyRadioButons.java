0b53b71d98f6fcbe95075d4727502e883949239f
Angel Jara <angeltorija4@gmail.com>
2022-09-19 12:25:47 +0200

    Nueva practica de botones con iconos, tambien incluyo los jpg

--- src/org/iesfm/ventana/MyRadioButons.java
+++ src/org/iesfm/ventana/MyRadioButons.java
@@ -2,30 +2,80 @@ package org.iesfm.ventana;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MyRadioButons {
 
     public static void main(String[] args) {
         JFrame f = new JFrame();
-        f.setBounds(900, 500, 500, 200);
+        f.setBounds(700, 200, 450, 450);
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel mainPanel = new JPanel();
-        mainPanel.setBackground(Color.CYAN);
 
-        JRadioButton button = new JRadioButton("Aceptar");
-        button.setBounds(10, 10, 100, 100);
-        button.setBackground(Color.MAGENTA);
+        ButtonGroup buttons = new ButtonGroup();
 
-        Button button2 = new Button("Rechazar");
-        button2.setBounds(20, 10, 100, 100);
-        button2.setBackground(Color.MAGENTA);
+        JRadioButton linuxButton = new JRadioButton("Linux" ,true);
+        linuxButton.setActionCommand("Linux");
+        JRadioButton windowsButton = new JRadioButton("Windows");
+        windowsButton.setActionCommand("Windows");
+        JRadioButton macButton = new JRadioButton("Macintosh");
+        macButton.setActionCommand("Macintosh");
 
-        mainPanel.add(button);
-        mainPanel.add(button2);
-        f.add(mainPanel);
+        buttons.add(linuxButton);
+        buttons.add(windowsButton);
+        buttons.add(macButton);
 
+        mainPanel.add(linuxButton);
+        mainPanel.add(windowsButton);
+        mainPanel.add(macButton);
+
+        //Creacion de los iconos
+
+
+        JLabel image = new JLabel();
+        image.setIcon(new ImageIcon("Linux.png"));
+
+        mainPanel.add(image);
+
+
+        //Creacion boton OK
+        JButton okButton = new JButton("OK");
+        okButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+                String selected = buttons.getSelection().getActionCommand();
+
+                JOptionPane.showMessageDialog(f, "Has seleccionado " + selected);
+
+                //Se tendrian que llamar igual para poder hacer el truquito
+                image.setIcon(new ImageIcon(selected + ".jpg"));
+
+
+                //Ejemplo con ifs, es mejor el ejemplo anterior
+               /* if (linuxButton.isSelected()){
+                    JOptionPane.showMessageDialog(f,"Has seleccionado Linux");
+                }
+                else if(windowsButton.isSelected()){
+                    JOptionPane.showMessageDialog(f,"Has seleccionado Windows");
+                } else if (macButton.isSelected()) {
+                    JOptionPane.showMessageDialog(f,"Has seleccionado Mac");
+                }
+
+                //La f es la ventana que se queda bloqueada hasta que pulsamos otra vez
+                JOptionPane.showMessageDialog(f,"Has pulsado el boton");
+                 */
+            }
+        });
+        mainPanel.add(okButton);
+
+        f.setContentPane(mainPanel);
+
+        f.repaint();
+        f.revalidate();
 
     }
 }
