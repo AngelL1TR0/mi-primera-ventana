48fe5d653c3e85aa93f0db5ddf020684f9d5e52a
Angel Jara <angeltorija4@gmail.com>
2022-09-27 10:43:25 +0200

    MyBorderLayoutWindow

--- src/org/iesfm/ventana/MyBorderLayoutWindow.java
+++ src/org/iesfm/ventana/MyBorderLayoutWindow.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class MyBorderLayoutWindow {
 
     public static void main(String[] args) {
-        JFrame f = new JFrame();
+        JFrame f = new JFrame("MyBorderLayoutWindow");
         f.setBounds(
                 10, 10, 550, 300
         );
@@ -14,13 +14,19 @@ public class MyBorderLayoutWindow {
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel mainPanel = new JPanel();
-        f.setLayout(new FlowLayout());
 
-        mainPanel.add(new JButton("Hide North Border"), BorderLayout.NORTH);
-        mainPanel.add(new JButton("Hide South Border"), BorderLayout.SOUTH);
-        mainPanel.add(new JButton("Hide West Border"), BorderLayout.WEST);
-        mainPanel.add(new JButton("Hide East Border"), BorderLayout.EAST);
-        mainPanel.add(new JButton("Hide Center Border"), BorderLayout.CENTER);
+        mainPanel.setLayout(new BorderLayout(10,10));
+
+        JButton north = new JButton("Hide North Border");
+        mainPanel.add(north, BorderLayout.NORTH);
+        JButton south = new JButton("Hide South Border");
+        mainPanel.add(south, BorderLayout.SOUTH);
+        JButton center = new JButton("Hide Center Border");
+        mainPanel.add(center, BorderLayout.CENTER);
+        JButton east = new JButton("Hide East Border");
+        mainPanel.add(east, BorderLayout.EAST);
+        JButton west = new JButton("Hide West Border");
+        mainPanel.add(west, BorderLayout.WEST);
 
         f.setContentPane(mainPanel);
 
