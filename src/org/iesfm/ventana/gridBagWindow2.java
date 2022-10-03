83b65354b10207b4c0fe51566864af3cf91971a9
AngelClase <angel.torijasanchezj@iesfm.onmicrosoft.com>
2022-10-03 12:02:52 +0200

    Ventana2

--- src/org/iesfm/ventana/gridBagWindow2.java
+++ src/org/iesfm/ventana/gridBagWindow2.java
@@ -16,16 +16,79 @@ public class gridBagWindow2 {
 
         JPanel mainPanel = new JPanel( new java.awt.GridBagLayout());
 
-        mainPanel.add(new JButton("Boton1"),
+        mainPanel.add(new JTextArea("Area Texto"),
                 new GridBagConstraints(
                         0,
                         0,
-                        1,
-                        1,
+                        2,
+                        2,
                         1.0,
-                        1.0,
+                        4,
                         GridBagConstraints.CENTER,
                         GridBagConstraints.BOTH,
+                        new Insets(0,0,0,0),
+                        0,0));
+
+        mainPanel.add(new JButton("Boton 1"),
+                new GridBagConstraints(
+                        2,
+                        0,
+                        1,
+                        1,
+                        0.5,
+                        2.0,
+                        GridBagConstraints.NORTH,
+                        GridBagConstraints.HORIZONTAL,
+                        new Insets(0,0,0,0),
+                        0,0));
+
+
+        mainPanel.add(new JButton("Boton 2"),
+                new GridBagConstraints(
+                        2,
+                        1,
+                        1,
+                        1,
+                        0.5,
+                        2.0,
+                        GridBagConstraints.NORTH,
+                        GridBagConstraints.HORIZONTAL,
+                        new Insets(0,0,0,0),
+                        0,0));
+        mainPanel.add(new JButton("Boton 3"),
+                new GridBagConstraints(
+                        0,
+                        2,
+                        1,
+                        1,
+                        0.5,
+                        0.5,
+                        GridBagConstraints.CENTER,
+                        GridBagConstraints.HORIZONTAL,
+                        new Insets(0,0,0,0),
+                        0,0));
+        mainPanel.add(new JTextField("Campo Texto"),
+                new GridBagConstraints(
+                        1,
+                        2,
+                        1,
+                        1,
+                        2,
+                        0.5,
+                        GridBagConstraints.CENTER,
+                        GridBagConstraints.HORIZONTAL,
+                        new Insets(0,0,0,0),
+                        0,0));
+        mainPanel.add(new JButton("Boton 4"),
+                new GridBagConstraints(
+                        2,
+                        2,
+                        1,
+                        1,
+                        0.5,
+                        0.5,
+                        GridBagConstraints.CENTER,
+                        GridBagConstraints.HORIZONTAL,
                         new Insets(0,0,0,0),
                         0,0));
 
